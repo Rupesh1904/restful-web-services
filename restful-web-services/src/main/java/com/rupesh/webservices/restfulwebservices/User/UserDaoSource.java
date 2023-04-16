5083516c565935b2c669948f6183d0dc2135a70f
Rupesh agrawal <rupesh.agrawal1904@outlook.com>
2023-04-17 02:46:17 +0530

    Making FIRST REST API

---
+++ restful-web-services/src/main/java/com/rupesh/webservices/restfulwebservices/User/UserDaoSource.java
@@ -0,0 +1,33 @@
+package com.rupesh.webservices.restfulwebservices.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoSource {
+    private static List<User> users= new ArrayList<>();
+    private static int usersCounts=0;
+
+    static {
+        users.add(new User( ++usersCounts, "Rupesh",LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCounts, "Avni",LocalDate.now().minusYears(30)));
+        users.add(new User( ++usersCounts, "Agrawal",LocalDate.now().minusYears(30)));
+    }
+    public List<User> findAll(){
+        return users;
+    }
+    public User findOne(int id){
+        Predicate<? super User> predicate= user-> user.getId().equals(id);
+        return  users.stream().filter(predicate).findFirst().get();
+    }
+    public User save(User user){
+        user.setId(++usersCounts);
+        users.add(user);
+        return user;
+    }
+
+}
